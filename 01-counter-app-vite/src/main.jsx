6c6f75bbae80a3c1adea8b592cd3907821c6f068
mgutbor <mgutbor@gmail.com>
2023-11-11 09:28:36 +0100

    useState

--- 01-counter-app-vite/src/main.jsx
+++ 01-counter-app-vite/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { FirstApp } from './FirstApp'
+import { CounterApp } from './CounterApp'
 
 import './styles.css'
 
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        <FirstApp />
+        <CounterApp value={ 20 }/>
     </React.StrictMode>
 )

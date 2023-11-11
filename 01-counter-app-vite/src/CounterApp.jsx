133e5dfee1bc5b7ddb3b2032d619115c87e6ecaa
mgutbor <mgutbor@gmail.com>
2023-11-11 09:37:46 +0100

    handleSubtrack y handleReset

--- 01-counter-app-vite/src/CounterApp.jsx
+++ 01-counter-app-vite/src/CounterApp.jsx
@@ -5,7 +5,15 @@ export const CounterApp = ({ value }) => {
     
     const [ counter, setCounter ] = useState( value );
     const handleAdd = () => { 
-        setCounter ( counter + 1);
+        setCounter ( counter + 1 );
+        // setCounter ( (c) => c + 1);
+    }
+    const handleSubtract = () => { 
+        setCounter ( counter - 1 );
+        // setCounter ( (c) => c + 1);
+    }
+    const handleReset = () => { 
+        setCounter ( value );
         // setCounter ( (c) => c + 1);
     }
     
@@ -13,9 +21,9 @@ export const CounterApp = ({ value }) => {
     <>
         <h1>CounterApp</h1>
         <h2> { counter } </h2>
-        <button onClick={ handleAdd }> 
-            +1 
-        </button>
+        <button onClick={ handleAdd }> +1 </button>
+        <button onClick={ handleSubtract }> -1 </button>
+        <button onClick={ handleReset }> Reset </button>
     </>
   )
 }

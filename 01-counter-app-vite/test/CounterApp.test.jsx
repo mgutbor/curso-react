df0d158026318bbb0594100a9f8ba1db7a7bbdec
mgutbor <mgutbor@gmail.com>
2023-11-14 19:33:52 +0100

    5.17 - simular eventos (click)

--- 01-counter-app-vite/test/CounterApp.test.jsx
+++ 01-counter-app-vite/test/CounterApp.test.jsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas sobre el componente CounterApp', () => {
 
-    test('debe hacer match con el snapshot', async() => {
+    const initialValue = 10;
 
-        const initialValue = 10;
+    test('debe hacer match con el snapshot', async() => {
 
         const { container } = render( <CounterApp value={initialValue} /> )
 
@@ -17,6 +17,33 @@ describe('Pruebas sobre el componente CounterApp', () => {
         render( <CounterApp value={100} /> );
         expect(screen.getByText(100)).toBeTruthy();
         // expect(screen.getByRole('heading', { level: 2}).innerHTML).toContain('100')
+    });
+
+    test('debe incrementar con el boton +1', () => {
+        
+        render( <CounterApp value={initialValue} /> );
+        fireEvent.click(screen.getByText('+1'))
+        expect(screen.getByText('11')).toBeTruthy();
+    });
+
+    test('debe decrementar con el boton -1', () => {
+        
+        render( <CounterApp value={initialValue} /> );
+        fireEvent.click(screen.getByText('-1'))
+        expect(screen.getByText('9')).toBeTruthy();
+    });
+
+    test('debe volver al valor inicial con el boton Reset', () => {
+        
+        render( <CounterApp value={initialValue} /> );
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('-1'));
+        fireEvent.click(screen.getByText('Reset'));
+
+        expect(screen.getByText('10')).toBeTruthy();
+
+        // screen.debug();
     });
 
 });

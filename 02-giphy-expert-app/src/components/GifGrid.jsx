1df2782859d0943d0240aff562d7de0af16d06d2
mgutbor <mgutbor@gmail.com>
2023-11-30 20:03:29 +0100

    6.14 - GiphyExpertApp - className

--- 02-giphy-expert-app/src/components/GifGrid.jsx
+++ 02-giphy-expert-app/src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getGif } from "../helpers/getGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ( { category } ) => {
 
@@ -13,12 +14,16 @@ export const GifGrid = ( { category } ) => {
   return (
     <>
       <h3>{ category }</h3>
-      <ol>
-      { images.map( ({id, title}) => {
-          return <li key={ id }>{ title }</li>
-        })
+      <div className="card-grid">
+      { 
+        images.map( ( image ) => (
+          <GifGridItem 
+            key={image.id}
+            {...image}
+          />
+        ))
       }
-      </ol>
+      </div>
     </>
   )
 }

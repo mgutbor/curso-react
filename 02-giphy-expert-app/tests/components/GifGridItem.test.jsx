58b0bdbcc03a287ca004b342a8ee3cc141282d7f
mgutbor <mgutbor@gmail.com>
2023-12-02 11:58:04 +0100

    8.3 - Tests - test en GifGridItem

--- 02-giphy-expert-app/tests/components/GifGridItem.test.jsx
+++ 02-giphy-expert-app/tests/components/GifGridItem.test.jsx
@@ -1,11 +1,11 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifGridItem } from '../../src/components/GifGridItem';
 
 
 describe('Pruebas en GifGridItem', () => {
   
   const title = 'Manolo';
-  const url = 'https://manolo.com'
+  const url = 'https://manolo.com/';
 
   test('debe hacer match con el snapshot', () => {
   
@@ -13,6 +13,27 @@ describe('Pruebas en GifGridItem', () => {
 
     expect( container ).toMatchSnapshot();
     
+  });
+
+  test('debe mostrar la imagen con el URL y el ALT indicado', () => {
+
+    render( <GifGridItem title={ title } url={ url } /> );
+    // screen.debug();
+
+    const {src, alt} = screen.getByRole('img');
+
+    // expect( screen.getByRole('img').src ).toBe( url );
+    expect( src ).toBe( url );
+    expect( alt ).toBe( title );
+
+  });
+
+  test('el texto dentro del parrafo ha de ser igual al indicado', () => {
+    
+    render( <GifGridItem title={ title } url={ url } /> );
+
+    expect( screen.getByText(title)).toBeTruthy();
+
   });
 
 });

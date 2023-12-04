81fcfdf9708d2a2f512c69621e55368d8fd53333
mgutbor <mgutbor@gmail.com>
2023-12-04 20:33:51 +0100

    8.9 - Tests - mock completo de customHook

--- 02-giphy-expert-app/tests/components/GifGrid.test.jsx
+++ 02-giphy-expert-app/tests/components/GifGrid.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
 
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en GifGrid', () => {
   
@@ -8,12 +10,44 @@ describe('Pruebas en GifGrid', () => {
 
   test('debe mostrar el loading inicialmente', () => {
     
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true
+    });
+
     render( <GifGrid category={ category } /> );
 
     //  screen.debug();
 
     expect( screen.getByText('Cargando...') );
     expect( screen.getByText(category) );
+
+  });
+
+  test('debe mostrar items cuando se cargan las imagenes desde fetchGifs', () => {
+    
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'ABC',
+        url: 'https://test1.com'
+      },
+      {
+        id: 'ABC2',
+        title: 'ABC2',
+        url: 'https://test2.com'
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: false
+    });
+
+    render( <GifGrid category={ category } /> );
+
+    screen.debug();
+
+    expect( screen.getAllByRole('img').length).toBe(2);
 
   });
 

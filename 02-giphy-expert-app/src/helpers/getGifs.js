32f5d8163e2e5edf13064d228a171e1779f5e49e
mgutbor <mgutbor@gmail.com>
2023-12-07 17:38:30 +0100

    delete gyphy API

--- 02-giphy-expert-app/src/helpers/getGifs.js
+++ 02-giphy-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGif = async(category) => {
 
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=QQfJXr0iRzFSrEi1l99KEbSH6h4a8XCm&q=${category}&limit=10`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${category}&limit=10`;
   const resp = await fetch( url );
   const { data } = await resp.json();
   
